19822e979384258c45b8dc65c63ad49efa84f480
KotaroSawasato <mlo123349@gmail.com>
2015-05-03 11:30:11 +0900

    Create 06.php

---
+++ 06.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<meta charset='UTF-8'>
+</head>
+<body>
+	学校名&nbsp;&nbsp;&nbsp;：<?php echo $_POST['data01'];?><br> 
+	学籍番号：<?php echo $_POST['data02'];?><br>
+	名前	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;：<?php echo $_POST['data03'];?><br>
+	自己紹介：<?php echo $_POST['data06'];?><br>
+	性別	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;：<?php echo $_POST['data04'];?><br>
+	学年	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;：<?php echo $_POST['grade'];?><br>	
+</body>
+</html>
