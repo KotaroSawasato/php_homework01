c9fe81d119e8e3ea91c60d677219e207fa0b0695
KotaroSawasato <mlo123349@gmail.com>
2015-05-03 11:42:28 +0900

    Update 01.php

--- 01.php
+++ 01.php
@@ -5,7 +5,6 @@
 <body>
 	<form method='post' action='02.php'>
 		学校名：<input type='text' name='school_name'/>
-		<input type='hidden' name='data01' value='<?php echo $_POST['school_name']; ?>'/>
 		<input type='submit'/>
 	</form>
 	<hr />
